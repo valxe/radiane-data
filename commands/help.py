b69d0fbba894593d1ed906bfae678f94a2276776
Valentine <111288675+valxe@users.noreply.github.com>
2024-06-01 12:24:44 +0200

    Update help.py

--- commands/help.py
+++ commands/help.py
@@ -3,13 +3,14 @@ import discord
 async def send_help_embed(channel):
     help_commands = {
         'user': "Usage: `!user {username}` - Retrieves information about the user's message activity.",
+        'random': "Usage: `!random` - Retrieves information about a random user.",
         'top': "Usage: `!top` - Displays the top 10 users by number of messages.",
         'total': "Usage: `!total` - Displays the total number of messages logged by the bot.",
-        'remove': "Usage: `!remove` - Remove an user from the database.",
+        'remove': "Usage: `!remove {username}` - Removes a user from the database.",
         'bl': "Usage: `!bl` - Lists all blacklisted users.",
         'bladd': "Usage: `!bladd {username}` - Adds a user to the blacklist. - Admin Only",
-        'blremove': "Usage: `!blremove {username}` - Removes a user from the blacklist. - Admin Only"
-        }
+        'blremove': "Usage: `!blremove {username}` - Removes a user from the blacklist. - Admin Only",
+    }
 
     embed = discord.Embed(title="Command Help", color=discord.Color.blue())
 
